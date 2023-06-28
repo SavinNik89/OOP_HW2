3cdfcda7aeef314e953cea8e6286a6cefb1af451
Savin <savinn1989@mail.ru>
2023-06-28 21:12:11 +1000

    hw5

---
+++ src/Home_Work_5/presenters/Model.java
@@ -0,0 +1,21 @@
+package Home_Work_5.presenters;
+
+import Home_Work_5.models.Table;
+
+import java.util.Collection;
+import java.util.Date;
+
+public interface Model {
+    Collection<Table> loadTables();
+
+    /**
+     * Бронирование столика
+     * @param reservationDate Дата бронирования
+     * @param tableNo Номер столика
+     * @param name Имя клиента
+     */
+    int reservationTable(Date reservationDate, int tableNo, String name);
+
+    int changeReservationTable(int oldReservation, Date orderDate, int tableNo, String name);
+}
+
